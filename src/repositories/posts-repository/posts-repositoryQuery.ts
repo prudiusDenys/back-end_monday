aa66d30fc17db1972f9b93bc31b376b8c283e450
denysprudius <prdslife@gmail.com>
2022-10-10 12:32:00 +0200

    add queries parameters to the posts

--- src/repositories/posts-repository/posts-repositoryQuery.ts
+++ src/repositories/posts-repository/posts-repositoryQuery.ts
@@ -1,9 +1,33 @@
 import {Post} from '../../utils/interfaces';
 import {homework3Posts} from '../db';
+import {calcPagesCount, calcSkipPages} from '../../utils/calculatePagination';
 
 export const postsRepositoryQuery = {
-  async getAllPosts(): Promise<Post[]> {
-    return homework3Posts.find({}).toArray()
+  async getAllPosts(data: any) {
+    const {
+      sortBy = 'createdAt',
+      sortDirection = 'desc',
+      pageNumber = 1,
+      pageSize = 10
+    } = data
+
+    const totalCount = await homework3Posts.countDocuments()
+
+    const items = await homework3Posts
+      .find({})
+      .skip(calcSkipPages(+pageNumber, +pageSize))
+      .limit(+pageSize)
+      .sort({sortBy: sortDirection == 'asc' ? 1 : -1})
+      .toArray()
+
+    return {
+      pageSize,
+      pageNumber,
+      totalCount,
+      items,
+      pagesCount: calcPagesCount(totalCount, +pageSize)
+    }
+
   },
   async findPost(id: string): Promise<Post | null> {
     return homework3Posts.findOne({id})
--- src/routes/posts-router.ts
+++ src/routes/posts-router.ts
@@ -1,15 +1,15 @@
 import {Request, Response, Router} from 'express';
 import {authMiddleware} from '../middlewares/authMiddleware';
-import {removeMongoId} from '../utils/normalizeData';
+import {normalizeAllBlogsAndPosts, removeMongoId} from '../utils/normalizeData';
 import {postsRepositoryQuery} from '../repositories/posts-repository/posts-repositoryQuery';
 import {postsService} from '../services/posts-service';
 
 export const postsRouter = Router({})
 
 postsRouter.get('/', async (req: Request, res: Response) => {
-  const posts = await postsRepositoryQuery.getAllPosts()
+  const data = await postsRepositoryQuery.getAllPosts(req.query)
 
-  const normalizedPosts = removeMongoId(posts)
+  const normalizedPosts = normalizeAllBlogsAndPosts(data)
 
   res.status(200).json(normalizedPosts)
 })
