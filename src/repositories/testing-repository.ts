1f26f24d1337efed1341385653ae306cb1ccdb65
Denys Prudius <prdslife@gmail.com>
2022-11-15 18:20:41 +0100

    videos

--- src/repositories/testing-repository.ts
+++ src/repositories/testing-repository.ts
@@ -1,9 +1,10 @@
 import {comments, blogs, posts, users} from './db';
+import {videos} from '../routes/video-router';
 
 
 export const testingRepository = {
   async removeAllData() {
-    // videos.splice(0, videos.length)
+    videos.splice(0, videos.length)
     await posts.deleteMany({})
     await blogs.deleteMany({})
     await users.deleteMany({})
