30f23d0246f0d6a443d436e77cfd758d59ef513b
Denys Prudius <prdslife@gmail.com>
2022-12-28 19:48:35 +0100

    minor changes

--- src/repositories/comments-repository/comments-repositoryQuery.ts
+++ src/repositories/comments-repository/comments-repositoryQuery.ts
@@ -1,9 +1,10 @@
 import {Comment} from '../../utils/interfaces';
 import {Comments} from '../../mongoose/models';
 
-
-export const commentsRepositoryQuery = {
-  async findComment(commentId: string):Promise<Comment | null> {
+class CommentsRepositoryQuery {
+  async findComment(commentId: string): Promise<Comment | null> {
     return Comments.findOne({id: commentId}).select('-__v -_id -parentId')
   }
 }
+
+export const commentsRepositoryQuery = new CommentsRepositoryQuery()
